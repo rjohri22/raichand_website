5a1498a2cac66edf1cf92e35775038189600cad5
Preetu098 <preetu098@gmail.com>
2022-11-14 16:28:29 +0530

    add changes

--- portal/footer.php
+++ portal/footer.php
@@ -7,19 +7,11 @@
                 </div>
                 <div class="col-lg-7 col-md-7">
                     <ul class="menu">
-<<<<<<< HEAD
                        <li><a href="<?php echo SITE_URL;?>/portal/press-release.php">Press Release</a></li> &nbsp;&nbsp;|
                         <li><a href="<?php echo SITE_URL;?>/portal/faq.php">FAQ </a></li> &nbsp;&nbsp;|
                         <li><a href="<?php echo SITE_URL;?>/portal/policies.php">Policies </a></li> &nbsp;&nbsp;|
                         <li><a href="<?php echo SITE_URL;?>/portal/investor-relations.php">Investor Relations</a></li>&nbsp;&nbsp;|
                         <li><a href="<?php echo SITE_URL;?>/careers.php" class="w-100">Careers</a></li>
-=======
-                       <li><a href="<?php echo SITE_URL;?>portal/press-release.php">Press Release</a></li> &nbsp;&nbsp;|
-                        <li><a href="<?php echo SITE_URL;?>portal/faq.php">FAQ </a></li> &nbsp;&nbsp;|
-                        <li><a href="<?php echo SITE_URL;?>portal/policies.php">Policies </a></li> &nbsp;&nbsp;|
-                        <li><a href="<?php echo SITE_URL;?>portal/investor-relations.php">Investors Relation</a></li>&nbsp;&nbsp;|
-                        <li><a href="<?php echo SITE_URL;?>careers.php" class="w-100">Careers</a></li>
->>>>>>> 291ead172922cf8945dcb88717d8265fcb0d358c
 
                     </ul>
                     <!--Scroll to top Two-->
